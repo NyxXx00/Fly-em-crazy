e2ec38331c2bf519169185758626c3fbbb480695
LINKER-4134 <danielhernandezsp@gmail.com>
2026-07-19 18:49:13 +0200

    montaje menu y primer nivel sin minijuegos

--- Assets/Scripts/Minigame1/Minigame1Manager.cs
+++ Assets/Scripts/Minigame1/Minigame1Manager.cs
@@ -5,6 +5,14 @@ public class Minigame1Manager : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private bool musicStarted = false;
+    private AudioSource musicSource;
+    [SerializeField] private AudioClip musicClip;
+    [Header("Sleeping")]
+
+    [SerializeField] private bool sleep = false;
+    [SerializeField] private float wakeUpThreshold;
+
 
     [Header("Stress")]
 
@@ -42,7 +50,9 @@ public class Minigame1Manager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        musicSource = GetComponent<AudioSource>();
+        musicSource.clip = musicClip;
+        musicSource.clip.LoadAudioData();
         personSR= person.GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         timer = timeUntilDecay;
@@ -62,12 +72,17 @@ public class Minigame1Manager : MonoBehaviour
 
         stressBar.value = stressAmount;
 
-        personSR.sprite = personPortraits[(int)((personPortraits.Length-1) * stressAmount)];
+        if (!sleep)
+        {
+            personSR.sprite = personPortraits[(int)((personPortraits.Length - 1) * stressAmount)];
 
 
-        attackTimer -= Time.deltaTime;
+            attackTimer -= Time.deltaTime;
 
-        if (attackTimer <= 0) TryAttack();
+            if (attackTimer <= 0) TryAttack();
+
+        }
+        else if (stressAmount >= wakeUpThreshold / 100) sleep = false;
         
 
     }
@@ -88,8 +103,19 @@ public class Minigame1Manager : MonoBehaviour
         finalScreen.SetActive(true); 
     }
 
+    
+
+    
+
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (!musicStarted)
+        {
+            musicStarted = true;
+            stressBar.gameObject.SetActive(true);
+            musicSource.Play();
+        }
+
         if (collision.CompareTag("Player"))
         {
             timer = timeUntilDecay;
